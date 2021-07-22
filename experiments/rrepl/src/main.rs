6b6346e894f4196ba9d59ac265f3be8862cbf55a
Thomas Getgood <thomas@macroexpanse.com>
2021-07-21 23:46:38 -0400

    A working, if primitive, reader.

--- experiments/rrepl/src/main.rs
+++ experiments/rrepl/src/main.rs
@@ -6,8 +6,8 @@
 enum Sexp {
     Bool(bool),
     Symbol(String),
-    Keyword(String),
-    String(String),
+    // Keyword(String),
+    // String(String),
     Number(i128),
     List(Vec<Sexp>),
     // Vector(Vector<Sexp>),
@@ -17,39 +17,6 @@ enum Sexp {
 
 impl Eq for Sexp {}
 
-// /// A Proper Lisp Reader doesn't tokenise, but instead reads character by
-// /// character so that we can do things like special dispatch, literals, reader
-// /// macros, etc.. This is an expedient, and a bit of a lazy one at that.
-// fn tokenise(code: String) -> Vec<String> {
-//     code.replace("(", " ( ")
-//         .replace(")", " ) ")
-//         .split_whitespace()
-//         .map(|x| x.to_string())
-//         .collect()
-// }
-
-// fn read_seq(tokens: &[String]) -> Result<Sexp, String> {
-//     let i = tokens.iter().position(|x| x == ")");
-//     match i {
-//         None => Err("unbalanced parens, no closing ')' found.".to_string()),
-//         Some(i) => {
-//             let bits = tokens[0..i]
-//                 .iter()
-//                 .map(|x| {
-//                     let v = Vec::new().clone_from_slice(x);
-//                     read(v)
-//                 })
-//                 .into_iter()
-//                 .collect();
-//             match bits {
-//                 Ok(v) => Sexp::List(v),
-//                 Err(m) => Err(m),
-//             }
-//         }
-//     }
-// }
-
-
 fn split(x: String, delim: String) -> (String, String) {
     (
         x.chars().take_while(|&x| !delim.contains(x)).collect(),
@@ -57,40 +24,50 @@ fn split(x: String, delim: String) -> (String, String) {
     )
 }
 
-fn chunk_inner(x: String, delim: String) -> Vec<String> {
-    if x.is_empty() {
-        return Vec::new()
+fn read_atom(x: String) -> Result<(Sexp, String), String> {
+    let (head, tail) = split(x, "()[]{}, ".to_string());
+    let v: Sexp;
+    if head == "true".to_string() {
+        v = Sexp::Bool(true);
+    } else if head == "false".to_string() {
+        v = Sexp::Bool(false);
     } else {
-        let (head, tail) = split(x, "({[, ".to_string());
-        let mut recur = chunk_inner(tail, delim);
-        if !head.is_empty() {
-            recur.push(head)
+        match head.parse::<i128>() {
+            Ok(num) => {
+                v = Sexp::Number(num);
+            }
+            _ => {
+                v = Sexp::Symbol(head);
+            }
         }
-        return recur
     }
-} 
 
-fn chunks(x: String, delim: String) -> Vec<String> {
-    let mut res = chunk_inner(x, delim);
-    res.reverse();
-    res
+    Ok((v, tail.trim().to_string()))
 }
 
-fn read_atom(x: String) -> Result<Sexp, String> {
-    Err("not implemented".to_string())
+fn read_seq(chars: String, acc: &mut Vec<Sexp>) -> Result<(Sexp, String), String> {
+    let mut c = chars.chars();
+    match c.next() {
+        Some(')') => Ok((Sexp::List(acc.clone()), c.collect::<String>())),
+        Some(']') => Err("Unexpected char in list ']'".to_string()),
+        None => Err("Unexpected end of input".to_string()),
+        _ => {
+            let (n, rest) = read0(chars)?;
+            acc.push(n);
+            read_seq(rest, acc)
+        }
+    }
 }
 
-fn read_seq(chars: String) -> Result<Sexp, String> {
-    let (seq, rest) = split(chars, ")".to_string());
-    // let chunk = chars.take_while(|x| !delimiters.contains(x));
-    Err("um".to_string())
-}
+fn read0(code: String) -> Result<(Sexp, String), String> {
+    let mut chars = code.chars();
+    let c = chars.next();
+    let rest = chars.collect::<String>();
 
-fn read_switch(code: String) -> Result<Sexp, String> {
-    match code.chars().next() {
+    match c {
         None => Err("unexpected end of input".to_string()),
-        Some('(') => read_seq(code),
-        _ => Err("um".to_string()),
+        Some('(') => read_seq(rest, &mut Vec::new()),
+        Some(_) => read_atom(code),
     }
 }
 
@@ -98,13 +75,13 @@ fn read(code: String) -> Result<Sexp, String> {
     if code.is_empty() {
         Err("no input".to_string())
     } else {
-        read_switch(code)
+        let (res, _) = read0(code.replace(",", " "))?;
+        Ok(res)
     }
 }
 
-
 fn main() {
-    let t = "(some test (expr))".to_string();
+    let t = "(some test (true 1230))".to_string();
     // let (a, b) = split_to_stop(t);
     println!("{:?}", read(t));
 }
