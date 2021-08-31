335a508c8e162148498f276bc9e0d2b8960096f9
Thomas Getgood <thomas@macroexpanse.com>
2021-08-31 10:24:49 -0400

    I need a better way to create these config structs.

--- experiments/clj-vulkan/src/clj_vulkan/core.clj
+++ experiments/clj-vulkan/src/clj_vulkan/core.clj
@@ -8,7 +8,12 @@
            [org.lwjgl.vulkan
             VK11
             VkApplicationInfo
+            VkDeviceQueueCreateInfo
+            VkPhysicalDeviceFeatures
+            VkDeviceCreateInfo
+            VkDevice
             VkInstance
+            VkQueue
             VkInstanceCreateInfo
             VkPhysicalDevice
             VkLayerProperties]))
@@ -23,8 +28,10 @@
   (when (GLFW/glfwInit)
     (GLFW/glfwCreateWindow (int width) (int height) title c/null c/null)))
 
-(defn teardown-vulkan [instance]
-  (VK11/vkDestroyInstance instance nil))
+(defn teardown-vulkan [{:keys [instance context] :as state}]
+  (VK11/vkDestroyDevice context nil)
+  (VK11/vkDestroyInstance instance nil)
+  nil)
 
 (defn teardown-glfw [window]
   (GLFW/glfwDestroyWindow window)
@@ -36,7 +43,7 @@
       (GLFW/glfwPollEvents)
       (recur))))
 
-(defn init-vulkan [{:keys [validation-layers]}]
+(defn create-instance [{:keys [validation-layers]}]
   (when (check-validation-layers validation-layers)
     (with-open [stack (MemoryStack/stackPush)]
       (let [appInfo    (VkApplicationInfo/callocStack stack)
@@ -68,17 +75,28 @@
 (defn bit-check [pos x]
   (odd? (unsigned-bit-shift-right x pos)))
 
-(defn suitable-device? [device]
+(defn suitable-queue-family? [qf]
   (let [gbit   (->> queue-flags
                     :values
                     (filter #(= "VK_QUEUE_GRAPHICS_BIT" (:name %)))
                     first
-                    :value)
-        queues (->> device
-                    lists/queue-families
-                    (map api/parse)
-                    (map :queueFlags))]
-    (some (partial bit-check gbit) queues)))
+                    :value)]
+    (bit-check gbit (:queueFlags qf))))
+
+(defn queue-family-index [device]
+  (->> device
+       lists/queue-families
+       (map api/parse)
+       (zipmap (range))
+       (filter (fn [[k v]] (suitable-queue-family? v)))
+       (map key)
+       first))
+
+(defn suitable-device? [device]
+  (->> device
+       lists/queue-families
+       (map api/parse)
+       (some suitable-queue-family?)))
 
 (defn physical-device [instance]
   (->> #(VK11/vkEnumeratePhysicalDevices instance %1 %2)
@@ -87,15 +105,48 @@
        (filter suitable-device?)
        first))
 
+(defn init-device [device]
+  (with-open [stack (MemoryStack/stackPush)]
+    (let [qfi      (queue-family-index device)
+          qc       (VkDeviceQueueCreateInfo/callocStack 1 stack)
+          df       (VkPhysicalDeviceFeatures/callocStack stack)
+          dc       (VkDeviceCreateInfo/callocStack stack)
+          context& (.pointers stack VK11/VK_NULL_HANDLE)]
+
+      (doto qc
+        (.sType VK11/VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO)
+        (.queueFamilyIndex qfi)
+        (.pQueuePriorities (.floats stack (float 1))))
+
+      (doto dc
+        (.sType VK11/VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO)
+        (.pQueueCreateInfos qc)
+        (.pEnabledFeatures df))
+
+      (when (VK11/vkCreateDevice device dc nil context&)
+        (let [context (VkDevice. (.get context& 0) device dc)
+              queue&  (.pointers stack VK11/VK_NULL_HANDLE)]
+          (VK11/vkGetDeviceQueue context qfi 0 queue&)
+          {:context context :queue (VkQueue. (.get queue& 0) context)})))))
+
+(defn init-vulkan [opts]
+  (let [instance                (create-instance opts)
+        device                  (physical-device instance)
+        {:keys [context queue]} (init-device device)]
+    {:instance instance
+     :device   device
+     :context  context
+     :queue    queue}))
+
+
 (defonce graphical-state (atom nil))
 
 (defn start! [config]
   (swap! graphical-state assoc :window (init-window (:window config)))
-  (swap! graphical-state assoc :instance (init-vulkan config))
-  (swap! graphical-state #(assoc % :device (physical-device (:instance %)))))
+  (swap! graphical-state assoc :vulkan (init-vulkan config)))
 
 (defn stop! []
-  (swap! graphical-state update :instance teardown-vulkan)
+  (swap! graphical-state update :vulkan teardown-vulkan)
   (swap! graphical-state update :window teardown-glfw))
 
 
