8e289511b61825e714d1db4a3ce51a298471160e
Thomas Getgood <thomas@macroexpanse.com>
2022-05-04 14:49:23 -0500

    Some basic types for sexps.

--- experiments/host-evaluation/julia/src/reader.jl
+++ experiments/host-evaluation/julia/src/reader.jl
@@ -1,18 +1,55 @@
+struct ReaderOptions
+    until::Char
+end
+
+abstract type Sexp end
+
+abstract type LispMap <: Sexp end
+
+abstract type LispList <: Sexp end
+
+abstract type LispVector <: Sexp end
+
+abstract type LispSet <: Sexp end
+
+struct LispString <: Sexp
+    val::AbstractString
+end
+
+struct LispNumber <: Sexp
+    val::Number
+end
+
+struct ArrayList <: LispList
+    elements::Vector{Sexp}
+end
+
+struct ArrayVector <: LispVector
+    elements::Vector{Sexp}
+end
+
+struct LispMapEntry
+    key::Sexp
+    value::Sexp
+end
+
+struct ArrayMap <: LispMap
+    kvs::Vector{LispMapEntry}
+end
+
 ## This will raise an error on EOF. That's normally the right behaviour, but we
 ## might need a softer try-read sort of fn.
 function read1(stream)
     return read(stream, Char)
 end
 
-struct ReaderOptions
-    endchar::Char
-end
-
 whitespace = r"[\s,]"
+
+iswhitespace(c) = match(whitespace, string(c)) !== nothing
 
 function firstnonwhitespace(stream)
     c::Char = ' '
-    while match(whitespace, string(c)) !== nothing
+    while iswhitespace(c)
         c = read1(stream)
     end
     return c
@@ -22,20 +59,43 @@ function interpret(x)
     x
 end
 
-function readlist(stream)
+function readlist(stream, opts)
+    ArrayList([])
 end
 
-dispatch = Dict(
-    '(' => readlist
-###     '[' => readvector,
-###     '"' => readstring,
-###     '{' => readmap,
-###     '#' => readdispatch
-)
+function readvector(stream, opts)
+    ArrayVector([])
+end
+
+function readstring(stream, opts)
+    ""
+end
+
+function readmap(stream, opts)
+    ArrayMap([])
+end
 
 indirectdispatch = Dict()
 
-function listreader(stream, opts)
+function readdispatch(stream, opts)
+end
+
+dispatch = Dict(
+    '(' => readlist,
+    '[' => readvector,
+    '"' => readstring,
+    '{' => readmap,
+    '#' => readdispatch
+)
+
+function readtoken(c, stream)
+    out = ""*c
+
+    while !iswhitespace(c)
+        c = read1(stream)
+
+function lispreader(stream, opts)
+    c = nothing
     try
         c = firstnonwhitespace(stream)
     catch EOFError
@@ -47,16 +107,10 @@ function listreader(stream, opts)
     if sub === nothing
         return interpret(readtoken(c, stdin))
     else
-        return sub(stream)
+        return sub(stream, opts)
     end
 end
 
 function test()
-    c = ' '
-    out = []
-    while c !== 'z'
-        c = read1(stdin)
-        push!(out, c)
-    end
-    return out
+    lispreader(stdin, ReaderOptions(')'))
 end
