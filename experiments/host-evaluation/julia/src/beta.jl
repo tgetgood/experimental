d5417784118f0da05e87adafa58545afd9a87e8e
Thomas Getgood <thomas@macroexpanse.com>
2022-06-15 10:57:07 -0400

    We have a data representation for networks.
    Suitable for extending and combining networks arbitrarily. Remains to be seen
    how suitable it is for executing networks.

--- experiments/host-evaluation/julia/src/beta.jl
+++ experiments/host-evaluation/julia/src/beta.jl
@@ -1,79 +1,108 @@
 # module Operators
 
-struct LispChannel <: Sexp
-    ch::Channel
-end
+import Base: string
+import Main.DataStructures: vec, map, nil, assoc, emptymap, hashmap, MapEntry, Map, into, first, rest, keyword, transduce, conj, reduce, merge, Keyword, interpose, name, empty, Vector, emptyvector, count
 
-function channel()
-    LispChannel(Channel())
-end
 
 struct Beta
-    ins::LispMap
-    outs::LispMap
+    ins
+    outs
     lambda
 end
+
+function string(x::Beta)
+    string(hashmap(keyword("in"), x.ins, keyword("out"), x.outs, keyword("body"), x.lambda))
+end
+
+struct Network
+    βs
+    wires
+end
+
+emptynetwork = Network(emptymap, emptyvector)
+
+function emptyp(x::Network)
+    x == emptynetwork
+end
+
+function string(x::Network)
+    string(hashmap(keyword("βs"), x.βs, keyword("wires"), x.wires))
+end
+
+state = keyword("state")
+in    = keyword("in")
+out   = keyword("out")
 
 # In order to be able to link up βs into a graph, we need to know what the in
 # and out channels are right now. There's nothing stipping us from replacing a β
 # with a new one which can emit to more places, but that's a new entity.
 function beta(in, out, body)
-    ins = emptymap
-    outs = emptymap
-
-    for x in in
-        assoc(ins, x, channel())
-    end
-
-    for x in out
-        assoc(outs, x, channel())
-    end
-
-    return Beta(ins, outs, body)
+    Beta(in, out, body)
 end
 
-function consumingfeedbackwire(in, out, β)
-    ins = copy!(β.ins)
-    outs = copy!(β.outs)
-
-    inch = β.ins[in].ch
-    ouch = β.outs[out].ch
-
-    @async begin
-        while true
-            v = take!(inch)
-            put!(ouch, v)
+function βmap(λ)
+    function inner(e)
+        function(x)
+            e(out, λ(get(x, in)))
         end
     end
 
-    delete!(ins, in)
-    delete!(outs, out)
-
-    beta(ins, outs, β.lambda)
+    Beta(vec(in), vec(out), inner)
 end
 
-state = LispKeyword(nil, "state")
-in    = LispKeyword(nil, "in")
-out   = LispKeyword(nil, "out")
+βid = βmap(identity)
 
-# TODO: How do we tell the runtime that the `:state` in channel receives from
-# the `:state` out channel? just using the names with magic conventions isn't an
-# appealing solution.
-#
-# What if we allow transducers to emit to their own inbound channels? That is,
-# if :state exists in the list if in channels but not in the out channels, the
-# β can emit to it, but normal processes cannot listen to it. Or can they? Being
-# able to inspect the state of processes will be invaluable to an inhabited
-# system.
-#
-# So should the self-referential channel be both an in and out channel with the
-# same name? Should it be a relation (name, in|out) instead of two sets?
-#
-# This is all overly complicated. A transduction network with a noop forwarder
-# can trivially construct loops.
-#
-# So a transducer with state is actually two (or more) transducers all but one
-# of which are just `(map identity)` looping an output back to an input.
+function extendnamespace(x::Keyword, name)
+    if x.namespace === nil
+        keyword(name, x.name)
+    else
+        keyword(name*"."*x.namespace, x.name)
+    end
+end
+
+function extendnamespace(x::MapEntry, name)
+    MapEntry(extendnamespace(x.key, name), x.value)
+end
+
+function extendnamespace(x::Map, name)
+    into(empty(x), map(x -> extendnamespace(x, name)), x)
+end
+
+function extendnamespace(x::Vector, name)
+    into(empty(x), map(x -> extendnamespace(x, name)), x)
+end
+
+function extendnamespace(net::Network, name)
+    Network(
+        extendnamespace(net.βs, name),
+        into(vec(), map(x -> extendnamespace(x, name)), net.wires)
+    )
+end
+
+function extendnamespace(x::MapEntry)
+    extendnamespace(x.value, name(x.key))
+end
+
+function mergenetworks(a::Network, b::Network)
+    Network(merge(a.βs, b.βs), into(a.wires, b.wires))
+end
+
+function mergenetworks(x::Network)
+    x
+end
+
+function mergenetworks()
+    emptynetwork
+end
+
+function mergenetworks(netmap::Map)
+    transduce(map(extendnamespace), mergenetworks, emptynetwork, netmap)
+end
+
+function wire(n, newwire)
+    Network(n.βs, conj(n.wires, newwire))
+end
+
 function pbody(e)
     function(inputs)
         s = get(inputs, state)
@@ -87,9 +116,21 @@ function pbody(e)
     end
 end
 
-id = beta([in], [out], e -> i -> e(get(i, in)))
-
-partition = consumingfeedbackwire(state, state, beta([in, state], [out, state], pbody))
+partition = Network(
+    hashmap(
+        keyword("main"), beta(vec(in, state), vec(out, state), pbody),
+        keyword("state"), βmap(identity)
+        ),
+    vec(
+        # REVIEW: :main/state here refers to both the in and out channels of the
+        # main partition transducer. There's no ambiguity since wires are
+        # directed, but is there a communication problem?
+        #
+        # I guess we can always wait and see if I get confused...
+        vec(keyword("main", "state"), keyword("state", "in")),
+        vec(keyword("state", "out"), keyword("main", "state"))
+    )
+)
 
 function dupi(e)
     function (x)
