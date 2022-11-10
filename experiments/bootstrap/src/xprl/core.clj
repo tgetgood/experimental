52dafa4085ec235064aba7e2a63d9ad0706ed8a9
Thomas Getgood <thomas@macroexpanse.com>
2022-11-10 12:03:32 -0500

    Attempt #176 to define the core of the interpreter via itself.

--- experiments/bootstrap/src/xprl/core.clj
+++ experiments/bootstrap/src/xprl/core.clj
@@ -1,4 +1,12 @@
-(ns xprl.core)
+(ns xprl.core
+  (:refer-clojure :exclude [eval]))
+
+(def trivial
+  "(defn f [x] (conj [] x)))")
+
+(def less-trivial
+  "(defn apply [env ^{quote fn} _ [args body]] ^Fn {:env env :args args :body body})")
+
 
 (defn eval-xprl [env form]
   )
